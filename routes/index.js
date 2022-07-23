49c486a08cb1798c80087e1c71af43666f4eaac6
ndthinh02 <thinhjerry1410@gmail.com>
2022-07-23 17:54:34 +0700

    tadb

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,15 @@
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
-var upload = require('multer');
+var multer = require('multer');
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads');
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + "-" + file.originalname);
+    }
+})
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -18,20 +26,22 @@ router.get('/getUser', function (req, res) {
         }
     })
 })
-router.post('/createUser',upload.single('avatar'),function (req,res){
+var upload = multer({storage: storage});
+router.post('/createUser', upload.single('avatar'), function (req, res) {
     var email = req.body.email
     var pass = req.body.pass;
     var data = {
-        email :undefined,
-        pass :undefined,
-        avatar:undefined,
-        urlAvatar:undefined
+        email: undefined,
+        pass: undefined,
+        avatar: undefined,
+        urlAvatar: undefined
     }
     data.email = email;
     data.pass = pass;
 
     data.avatar = req.file.originalname
     data.urlAvatar = req.file.path;
+    res.send(data);
 })
 
 
